338a49adafff2512fd3613855e5da21609c21c0c
Abhinand S <117514296+SupaStrikas1@users.noreply.github.com>
2025-10-26 12:09:10 +0530

    Create Lexical_Analyser.c

---
+++ Lab Cycle 1/Lexical_Analyser.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int isKeyword(char *w) {
+    char *kw[] = {"auto","break","case","char","const","continue","default","do","double","else","enum",
+                  "extern","float","for","goto","if","int","long","register","return","short","signed",
+                  "sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"};
+    for (int i = 0; i < 32; i++) if (!strcmp(w, kw[i])) return 1;
+    return 0;
+}
+
+int isOperator(char c) { return strchr("+-*/%=<>!", c) != NULL; }
+int isSeparator(char c) { return strchr("(){}[];,#", c) != NULL; }
+
+int main() {
+    char file[30], ch, buf[50]; int j = 0;
+    printf("Enter file name: "); scanf("%s", file);
+    FILE *fp = fopen(file, "r");
+    if (!fp) return printf("File not found.\n"), 1;
+
+    while ((ch = fgetc(fp)) != EOF) {
+
+        if (isalnum(ch) || ch == '_') buf[j++] = ch;
+        else {
+            if (j) {
+                buf[j] = '\0';
+                printf("%s is a %s.\n", buf, isKeyword(buf) ? "keyword" : "identifier");
+                j = 0;
+            }
+            if (!isspace(ch)) { 
+                if (isOperator(ch)) printf("%c is an operator.\n", ch);
+                else if (isSeparator(ch)) printf("%c is a separator.\n", ch);
+            }
+        }
+    }
+
+    if (j > 0) {
+        buf[j] = '\0';
+        printf("%s is a %s.\n", buf, isKeyword(buf) ? "keyword" : "identifier");
+    }
+
+    fclose(fp);
+    return 0;
+}
